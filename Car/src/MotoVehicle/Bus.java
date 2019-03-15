ce4b130179a0e2c2d68909181dc3f6ce89b49679
Bright071 <934248437@qq.com>
2019-03-15 10:48:12 +0800

    10.47

---
+++ Car/src/MotoVehicle/Bus.java
@@ -0,0 +1,26 @@
+package MotoVehicle;
+/**
+ * 
+ * 这个是10.47
+ * @author admin
+ *
+ */
+public class Bus extends MotoVehicle{
+	private int SeatCount;//座位
+	
+	public Bus() {}//空构造体
+	
+	//传参构造体
+	public Bus(int seatcount) {
+		this.SeatCount =seatcount;
+	}
+	@Override
+	public void CalcRent(int days) {
+		if(SeatCount>16) {//判断
+			System.out.println("总费用："+days*1500+"元");//计算并输出结果
+		}else {
+			System.out.println("总费用："+days*800+"元");//计算并输出结果
+		}
+		
+	}
+}
